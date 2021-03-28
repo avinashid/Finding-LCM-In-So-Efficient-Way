583da7677a1f2d1ec4e6cb02bb37037811cb7ae1
Avinash Kumar <45692994+avinashkumar532004@users.noreply.github.com>
2021-03-28 21:59:48 +0530

    Create LCM.java

--- LCM.java
+++ LCM.java
@@ -16,16 +16,12 @@ public class LCM2 {
 
 	public static void main(String[] args) {
 //		int[] main=MyArray.initializeArray();
-		long a = System.currentTimeMillis();
-		int[] main= {3434,4334,3433,34433,94,3,34,35,34,232,432,2423,232,4232,23,42,23,24,23,32,42,23,423,23,34,3,45,34,53,43,33,4,34,3,5,34,34,53,4,3,53,4,51,43,4,2,2,3,24,24,34,5,34,3,53,4,35,34,2,2,4,5,34,11323,5,34,34,2,3,24,2,232,42,4,23,24,43,43,4,3,64,5,46,7,5,77,56,57,565,6,5,6};
-//		System.out.print("The LCM of ");
-//		MyArray.printArray(main);
+		int[] main= {37,5,77,56,57,565,6,5,6};
+		System.out.print("The LCM of ");
+		MyArray.printArray(main);
 		long lcm = lcm(main);
 		System.out.print("\nIs : "+lcm+"\n");
-//		System.out.println(checkLCM(dup, lcm));
-		long b = System.currentTimeMillis();
-		System.out.println(b-a);
-//		System.out.println(checkLCM(main,lcm));
+//		System.out.println(checkLCM(main, lcm));
 	}
 	public static long lcm(int main[]) {
 		long lcm=1;
